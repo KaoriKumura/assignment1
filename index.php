2d85c092f44deeb9f9bf38a0c54d44ecc8855b2c
KaoriKumura <141163247+KaoriKumura@users.noreply.github.com>
2023-10-03 16:10:59 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
                 <li><a href="#">About Us</a></li>
                 <li><a href="#">Terms of Use</a></li>
                 <input class="search" type="text" placeholder="Search..">
+                <a class="move" href="comfirmation.php">Move to view the contents</a>
             </ul>
             </li>
         <ul>
@@ -27,27 +28,27 @@
             <h1>Registration</h1>
         </header>
         <main>
-         <form action="comfirmation.php" method="get" id="registration comfirmation" >
+         <form action="index.php" method="post">
         <section>
             <div class="info">
                 <label for="fname">First Name</label>
-                <input type="text" name="fname" id="fname" required size="25" maxlength="50" placeholder="First Name"/>
+                <input type="text" name="fname" id="fname" size="25" maxlength="50" placeholder="First Name"/>
             </div>
             <div class="info">
                         <label for="lname">Last Name</label>
-                        <input type="text" name="lname" id="lname" required size="25" maxlength="50" placeholder="Last Name"/>
+                        <input type="text" name="lname" id="lname" size="25" maxlength="50" placeholder="Last Name"/>
             </div>
             <div class="info">
                         <label for="address">Address</label>
-                        <input type="text" name="address" id="address" required size="30" maxlength="50">
+                        <input type="text" name="address" id="address" size="30" maxlength="50">
             </div>
             <div class="info">
                         <label for="phone">Phone</label>
-                        <input type="tel" name="phone" id="phone" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required>
+                        <input type="tel" name="phone" id="phone" pattern="[0-9]{3}[0-9]{3}[0-9]{4}">
             </div>
             <div class="info">
                         <label for="email">Email</label>
-                        <input type="email" name="email" id="email" required maxlength="30">
+                        <input type="email" name="email" id="email" maxlength="30">
             </div>  
         </section>
 
@@ -55,43 +56,6 @@
                <button type="submit">Submit</button>
              </form>
 
-             <?php
-             if($_SERVER['REQUEST_METHOD']='POST')
-             {  
-
-                 $fname = $_POST['fname'];
-                 $lname = $_POST['lname'];
-                 $address = $_POST['address'];
-                 $phone = $_POST['phone'];
-                 $email = $_POST['email'];
-                
-                 //Database Connectivity
-                 $servername="localhost";
-                 $username="root";
-                 $password="";
-                 $database="assignment one";
-     
-                 //creating a connection
-     
-                 $conn=mysqli_connect($servername, $username, $password, $database);
-                
-                 //die if connection failed
-                 if(!$conn)
-                 {
-                     die("Sorry, connection failed!".mysqli_connect_error());
-                 }
-                 else
-                 {
-                     //submit the insertion queries/data to database
-     
-                     $sql="INSERT INTO `registrations` (`fname`, `lname`, `address`, `phone`, `email`) 
-                     VALUES ('$fname', '$lname', '$address', '$phone', '$email')";
-                     $result=mysqli_query($conn, $sql);
-     
-                 }
-     
-             }
-?>
         </main>
 <div class="footer-basic">
     <footer>
@@ -105,5 +69,57 @@
         <p class="copyright">Kaori Kumura © 2023</p>
     </footer> 
          
+             <?php
+              if($_SERVER['REQUEST_METHOD']='POST')
+              {  
+                 $fname='';
+                 $lname='';
+                 $address='';
+                 $phone='';
+                 $email='';
+
+                 if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['address']) && isset($_POST['phone']) && isset($_POST['email']))
+                 {
+                     $fname=$_POST['fname'];
+                     $lname=$_POST['lname'];
+                     $address=$_POST['address'];
+                     $phone=$_POST['phone'];
+                     $email=$_POST['email'];
+                 }
+                //   $fname=$_POST['fname'];
+                //   $lname=$_POST['lname'];
+                //   $address=$_POST['address'];
+                //   $phone=$_POST['phone'];
+                //   $email=$_POST['email'];
+                
+                 //Database Connectivity
+                 $servername="localhost";
+                 $username="root";
+                 $password="";
+                 $database="assignment one";
+     
+                 //creating a connection
+     
+                 $conn=mysqli_connect($servername, $username, $password, $database);
+                //  $sql="SELECT * from `registrations`";
+                //  $registrations=mysqli_query($conn, $sql);
+                
+                 //die if connection failed
+                 if(!$conn)
+                 {
+                     die("Sorry, connection failed!".mysqli_connect_error());
+                 }
+                 else
+                 {
+                     //submit the insertion queries/data to database
+     
+                     $sql="INSERT INTO `registrations` (`fname`, `lname`, `address`, `phone`, `email`)
+                     VALUES ('$fname', '$lname', '$address', '$phone', '$email')";
+                     $result=mysqli_query($conn, $sql);
+     
+                 }
+                }
+        
+?>
     </body>
 </html>
